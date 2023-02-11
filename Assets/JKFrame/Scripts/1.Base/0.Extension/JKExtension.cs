3e987ac8de8a401dd6c2bca210080d9b2cbed329
橙飒 <634377685@qq.com>
2023-02-11 17:57:27 +0800

    新建工程

---
+++ Assets/JKFrame/Scripts/1.Base/0.Extension/JKExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+/// <summary>
+/// JKFrame框架主要的拓展方法
+/// </summary>
+public static class JKExtension
+{
+    #region 通用
+    /// <summary>
+    /// 获取特性
+    /// </summary>
+    public static T GetAttribute<T>(this object obj) where T:Attribute // 约束 T 必须是特性
+    {
+        return obj.GetType().GetCustomAttribute<T>();
+    }
+    
+    /// <summary>
+    /// 获取指定类型上的特性 => 需要外部判空
+    /// </summary>
+    /// <param name="type"/>特性所在的类型</param>
+    public static T GetAttribute<T>(this object obj, Type type) where T:Attribute // 约束 T 必须是特性
+    {
+        return type.GetCustomAttribute<T>();
+    }
+    #endregion
+
+}
---
+++ Assets/Test/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection; // 反射的命名空间
+using Sirenix.OdinInspector; // 使用Odin插件的特性要应用改命名空间
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+}
+
+
